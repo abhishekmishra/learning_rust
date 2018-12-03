f02f7a93141e21a0c18b38a9243ed8c12e31cc78
Abhishek Mishra <abhishekmishra3@yahoo.com>
2018-12-03 13:16:07 +0530

    added variables prj

---
+++ variables/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let a = [1, 2, 3, 4, 5];
+    let index = 10;
+
+    let element = a[index];
+
+    println!("The value of element is: {}", element);
+}
